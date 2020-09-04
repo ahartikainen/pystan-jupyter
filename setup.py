bf03cdc780ee18808f7411e16028153ec507cb85
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-09-04 13:55:41 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_long_description():
 setup(
     name="pystan-jupyter",
     license="ISC",
-    version="0.1b2",
+    version="0.1b3",
     description="Enable PyStan3 use on Jupyter Notebook/Lab.",
     author="Ari Hartikainen",
     url="https://github.com/ahartikainen/pystan-jupyter",
